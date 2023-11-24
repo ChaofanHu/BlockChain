0071385699c1df6d2fa9dd78e080dec3a83a220d
chaofan <chaofan.hu@usi.ch>
2023-11-24 17:27:58 +0100

    first commit

---
+++ src/test/java/pri/chaofan/blockchain/BlockChainApplicationTests.java
@@ -0,0 +1,13 @@
+package pri.chaofan.blockchain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockChainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
